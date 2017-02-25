4885ced239fa37e432e2ee139cbaac7820909320
dbertella <daniele.bertella@gmail.com>
2017-02-25 22:05:36 +0000

    Add redux and redux-cycles with some boilerlplate

--- src/index.js
+++ src/index.js
@@ -1,9 +1,38 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux';
+import { run } from '@cycle/xstream-run';
+import { createCycleMiddleware } from 'redux-cycles';
+
+// import {makeHTTPDriver} from '@cycle/http';
+// import {timeDriver} from '@cycle/time';
+import rootReducer, { main } from './reducer';
 import App from './App';
+
 import './index.css';
 
+import {
+  BrowserRouter as Router,
+} from 'react-router-dom';
+
+const cycleMiddleware = createCycleMiddleware();
+const { makeActionDriver } = cycleMiddleware;
+
+const store = createStore(
+  rootReducer,
+  applyMiddleware(cycleMiddleware)
+);
+
+run(main, {
+  ACTION: makeActionDriver()
+})
+
 ReactDOM.render(
-  <App />,
+  <Provider store={store}>
+    <Router>
+      <App />
+    </Router>
+  </Provider>,
   document.getElementById('root')
 );
