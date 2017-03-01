cbb16bf857d755c64d91167cb4d11bcc3a5ceb6b
dbertella <daniele.bertella@gmail.com>
2017-03-01 22:40:20 +0000

    Add service worker, static cache and dynamic content cache

---
+++ sw-precache-config.js
@@ -0,0 +1,24 @@
+module.exports = {
+  stripPrefix: 'build/',
+  staticFileGlobs: [
+    'build/*.html',
+    'build/manifest.json',
+    'build/static/**/!(*map*)'
+  ],
+  dontCacheBustUrlsMatching: /\.\w{8}\./,
+  swFilePath: 'build/service-worker.js',
+  ateFallback: 'index.html',
+  runtimeCaching: [{
+    urlPattern: /^https:\/\/public-api\.wordpress\.com\/wp\/v2\/sites\/foodanquote\.com\/.*/,
+    handler: 'networkFirst'
+  }, {
+    urlPattern: /^https:\/\/foodandquote\.files\.wordpress\.com\/.*/,
+    handler: 'fastest',
+    options: {
+        cache: {
+          maxEntries: 30,
+          name: 'images'
+        }
+    }
+  }]
+};
