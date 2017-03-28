0ec0013867d7d12b173a2ea106238b01a468d8f6
dbertella <daniele.bertella@gmail.com>
2017-03-29 00:36:39 +0100

    Updates

--- service-worker.js
+++ service-worker.js
@@ -37,7 +37,7 @@
 /* eslint-disable indent, no-unused-vars, no-multiple-empty-lines, max-nested-callbacks, space-before-function-paren, quotes, comma-spacing */
 'use strict';
 
-var precacheConfig = [["404.html","97039a901ee143c6e7c46ac5acd7064c"],["index.html","3578092b65b5e5bed7b77fa9cb75017e"],["manifest.json","472ee8c5a581ddf7d5b0a2b6b5dbc9aa"],["static/css/main.23ed97cb.css","69011d8b7afc3179abb5111ece921d86"],["static/js/main.5227cc92.js","7a87d63bab5954fa1a8b0c2dc6e35477"]];
+var precacheConfig = [["404.html","97039a901ee143c6e7c46ac5acd7064c"],["index.html","207a75d6491a0801faf312b1bcff3252"],["manifest.json","472ee8c5a581ddf7d5b0a2b6b5dbc9aa"],["static/css/main.23ed97cb.css","69011d8b7afc3179abb5111ece921d86"],["static/js/main.efa7d6a3.js","505eadb0f321c96681eb20dc7abca8ac"]];
 var cacheName = 'sw-precache-v3-sw-precache-' + (self.registration ? self.registration.scope : '');
 
 
